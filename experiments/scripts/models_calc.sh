453f19bcb27145ab757b441e96b929aba0bef15a
Pedro Zanineli <pedro.zanineli12@gmail.com>
2025-05-05 16:25:09 -0300

    orb v3

--- experiments/scripts/models_calc.sh
+++ experiments/scripts/models_calc.sh
@@ -10,20 +10,23 @@
 
 declare -A full_models
 
-full_models["deepmd"]="DPA3-v2-OpenLAM|DPA3-v2-MPtrj"
+# full_models["deepmd"]="DPA3-v2-OpenLAM|DPA3-v2-MPtrj"
 # full_models["fair-chem"]="eSEN-30M-OAM|eSEN-30M-MP|eqV2 M|eqV2 S DeNS"
-full_models["grace"]="GRACE-2L-OAM|GRACE-1L-OAM|GRACE-2L-MPtrj"
-full_models["mace"]="MACE-MPA-0|MACE-MP-0"
-full_models["mattersim"]="MatterSim-v1.0.0-5M"
-full_models["orb"]="ORB|ORB-MPTrj"
-full_models["sevenn"]="SevenNet-MF-ompa|SevenNet-l3i5"
+# full_models["grace"]="GRACE-2L-OAM|GRACE-1L-OAM|GRACE-2L-MPtrj"
+# full_models["mace"]="MACE-MPA-0|MACE-MP-0"
+# full_models["mattersim"]="MatterSim-v1.0.0-5M"
+# full_models["orb"]="ORB|ORB-MPTrj"
+# full_models["sevenn"]="SevenNet-MF-ompa|SevenNet-l3i5"
+
+full_models["orb"]="ORB-V3"
 
 source ~/.bashrc
 
 for category in "${!full_models[@]}"; do
     
     # echo "Categoria: $category"
-    conda activate $category
+    # conda activate $category
+    conda activate orb-v3
 
     IFS='|' read -r -a models <<< "${full_models[$category]}"
     
